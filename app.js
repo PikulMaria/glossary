6e3b826afafd7eb2a4c8873077ef844e7fddd831
Мария <79498918+PikulMaria@users.noreply.github.com>
2024-01-26 02:53:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ loadDictionary('my-dictionary.json', (err) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/api/define/:term', (req, res) => {
